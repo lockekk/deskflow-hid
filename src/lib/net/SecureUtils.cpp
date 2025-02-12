deskflow/deskflow

refactor: use vector<uint8_t> for keys

src/lib/net/SecureUtils.cpp
@@ -10,12 +10,9 @@
 
 namespace deskflow {
 
-std::string formatSSLFingerprint(const std::string &fingerprint, bool convertToHex, bool enableSeparators)
+std::string formatSSLFingerprint(const std::vector<uint8_t> &fingerprint, bool enableSeparators)
 {
-  std::string rtn = fingerprint;
-
-  if (convertToHex)
-    rtn = deskflow::string::toHex(fingerprint, 2);
+  std::string rtn = deskflow::string::toHex(fingerprint, 2);
 
   deskflow::string::uppercase(rtn);
 

lockekk/deskflow-hid

src/lib/net/SecureUtils.cpp
@@ -10,12 +10,9 @@
 
 namespace deskflow {
 
-std::string formatSSLFingerprint(const std::string &fingerprint, bool convertToHex, bool enableSeparators)
+std::string formatSSLFingerprint(const std::vector<uint8_t> &fingerprint, bool enableSeparators)
 {
-  std::string result = fingerprint;
-
-  if (convertToHex)
-    result = deskflow::string::toHex(fingerprint, 2);
+  std::string result = deskflow::string::toHex(fingerprint, 2);
 
   deskflow::string::uppercase(result);
 