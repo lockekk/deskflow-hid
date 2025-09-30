deskflow/deskflow

refactor: move no-restart to CoreArgs
chore: Client remove unused ClientArgs member m_args

src/lib/deskflow/CoreArgParser.cpp
@@ -85,6 +85,13 @@ void CoreArgParser::parse()
          (m_parser.value(CoreArgs::preventSleepOption) == "1"));
     Settings::setValue(Settings::Core::PreventSleep, value);
   }
+
+  if (m_parser.isSet(CoreArgs::restartOnFailureOption)) {
+    bool value =
+        ((m_parser.value(CoreArgs::restartOnFailureOption).toLower() == "true") ||
+         (m_parser.value(CoreArgs::restartOnFailureOption) == "1"));
+    Settings::setValue(Settings::Core::RestartOnFailure, value);
+  }
 }
 
 [[noreturn]] void CoreArgParser::showHelpText() const

lockekk/deskflow-hid

src/lib/deskflow/CoreArgParser.cpp
@@ -95,6 +95,13 @@ void CoreArgParser::parse()
     auto value = m_parser.value(CoreArgs::displayOption);
     Settings::setValue(Settings::Core::Display, value);
   }
+
+  if (m_parser.isSet(CoreArgs::restartOption)) {
+    bool value =
+        ((m_parser.value(CoreArgs::restartOption).toLower() == "true") ||
+         (m_parser.value(CoreArgs::restartOption) == "1"));
+    Settings::setValue(Settings::Core::RestartOnFailure, value);
+  }
 }
 
 [[noreturn]] void CoreArgParser::showHelpText() const