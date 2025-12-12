c98436bd219bfce8c82b804fa8a8d166a3c81592
Locke Huang <locke.huang@gmail.com>
2025-12-13 00:36:32 +1300

    Fix windows build that server can't detect mouse movement.

--- src/lib/deskflow/ServerApp.cpp
+++ src/lib/deskflow/ServerApp.cpp
@@ -405,7 +405,7 @@ deskflow::Screen *ServerApp::createScreen()
 {
 #if WINAPI_MSWINDOWS
   return new deskflow::Screen(
-      new MSWindowsScreen(true, !Settings::value(Settings::Core::UseHooks).toBool(), getEvents()), getEvents()
+      new MSWindowsScreen(true, Settings::value(Settings::Core::UseHooks).toBool(), getEvents()), getEvents()
   );
 #endif
 
