deskflow/deskflow

feat: add network monitoring and IP address management
- introduce NetworkMonitor class to track network addresses change
- replace QLineEdit with QComboBox for interface selection in settings
- implement automatic IP detection and suggestion logic
- add signal-slot connections for real-time network updates

Log: add network monitoring and IP address management.

translations/deskflow_es.ts
@@ -420,8 +420,16 @@ Do you want to connect to the server?
         <translation type="unfinished">No se puede detectar una dirección IP. Compruebe que su conexión de red esté activa.</translation>
     </message>
     <message>
-        <source>Suggested IP: %1</source>
-        <translation type="unfinished">IP sugerida: %1</translation>
+        <source>Using IP: </source>
+        <translation type="unfinished">Usando IP: </translation>
+    </message>
+    <message>
+        <source>Selected as the interface in settings.</source>
+        <translation type="unfinished">Seleccionado como la interfaz en la configuración.</translation>
+    </message>
+    <message>
+        <source>Suggested IP: </source>
+        <translation type="unfinished">IP sugerida: </translation>
     </message>
     <message>
         <source>&lt;p&gt;If connecting via the hostname fails, try %1&lt;/p&gt;</source>
@@ -434,6 +442,12 @@ Do you want to connect to the server?
     <message>
         <source>one of the following IPs:&lt;br/&gt;%1</source>
         <translation type="unfinished">una de las siguientes IP:&lt;br/&gt;%1</translation>
+    </message>
+    <message>
+        <source>
+A bound IP is now invalid, you may need to restart the server.</source>
+        <translation type="unfinished">
+La dirección IP asignada ahora no es válida; es posible que deba reiniciar el servidor.</translation>
     </message>
     <message>
         <source>&amp;File</source>
@@ -458,6 +472,12 @@ Do you want to connect to the server?
     <message>
         <source>invalid certificate, generating a new one</source>
         <translation type="unfinished">certificado no válido, generando uno nuevo</translation>
+    </message>
+    <message>
+        <source>
+Interface is not active. Unable to start server.</source>
+        <translation type="unfinished">
+La interfaz no está activa. No se puede iniciar el servidor.</translation>
     </message>
     <message>
         <source>%1 will retry in a moment...</source>
@@ -1254,6 +1274,10 @@ Al habilitar esta opción, se deshabilitará la interfaz gráfica de usuario (GU
     <message>
         <source>TLS Certificate Regenerated</source>
         <translation type="unfinished">Certificado TLS regenerado</translation>
+    </message>
+    <message>
+        <source>Any</source>
+        <translation type="unfinished">Cualquier</translation>
     </message>
     <message>
         <source>TLS certificate regenerated successfully.</source>

translations/deskflow_it.ts
@@ -408,8 +408,16 @@ Vuoi connetterti al server?
         <translation>Impossibile rilevare un indirizzo IP. Controlla che la tua connessione di rete sia attiva.</translation>
     </message>
     <message>
-        <source>Suggested IP: %1</source>
-        <translation>IP suggerito: %1</translation>
+        <source>Using IP: </source>
+        <translation type="unfinished">Utilizzo dell&apos;indirizzo IP: </translation>
+    </message>
+    <message>
+        <source>Selected as the interface in settings.</source>
+        <translation type="unfinished">Selezionata come interfaccia nelle impostazioni.</translation>
+    </message>
+    <message>
+        <source>Suggested IP: </source>
+        <translation type="unfinished">IP suggerito: </translation>
     </message>
     <message>
         <source>&lt;p&gt;If connecting via the hostname fails, try %1&lt;/p&gt;</source>
@@ -422,6 +430,12 @@ Vuoi connetterti al server?
     <message>
         <source>one of the following IPs:&lt;br/&gt;%1</source>
         <translation>uno dei seguenti IP:&lt;br/&gt;%1</translation>
+    </message>
+    <message>
+        <source>
+A bound IP is now invalid, you may need to restart the server.</source>
+        <translation type="unfinished">
+L&apos;indirizzo IP associato non è più valido, potrebbe essere necessario riavviare il server.</translation>
     </message>
     <message>
         <source>&amp;File</source>
@@ -466,6 +480,12 @@ Nomi validi:
     <message>
         <source>invalid certificate, generating a new one</source>
         <translation type="unfinished">certificato non valido, ne viene generato uno nuovo</translation>
+    </message>
+    <message>
+        <source>
+Interface is not active. Unable to start server.</source>
+        <translation type="unfinished">
+L&apos;interfaccia non è attiva. Impossibile avviare il server.</translation>
     </message>
     <message>
         <source>%1 will retry in a moment...</source>
@@ -1254,6 +1274,10 @@ L&apos;abilitazione di questa impostazione disabiliterà l&apos;interfaccia graf
     <message>
         <source>TLS Certificate Regenerated</source>
         <translation>Certificato TLS rigenerato</translation>
+    </message>
+    <message>
+        <source>Any</source>
+        <translation type="unfinished">Qualunque</translation>
     </message>
     <message>
         <source>TLS certificate regenerated successfully.</source>

translations/deskflow_ja.ts
@@ -372,8 +372,12 @@ Do you want to connect to the server?
         <translation>IPアドレスが見つかりません。ネットワーク接続を確認してください。</translation>
     </message>
     <message>
-        <source>Suggested IP: %1</source>
-        <translation>推奨IPアドレス: %1</translation>
+        <source>Using IP: </source>
+        <translation type="unfinished">IPアドレスを使用する： </translation>
+    </message>
+    <message>
+        <source>Selected as the interface in settings.</source>
+        <translation type="unfinished">設定でインターフェースとして選択されています。</translation>
     </message>
     <message>
         <source>&lt;p&gt;If connecting via the hostname fails, try %1&lt;/p&gt;</source>
@@ -386,6 +390,12 @@ Do you want to connect to the server?
     <message>
         <source>one of the following IPs:&lt;br/&gt;%1</source>
         <translation>以下のIPアドレスのいずれか:&lt;br/&gt;%1</translation>
+    </message>
+    <message>
+        <source>
+A bound IP is now invalid, you may need to restart the server.</source>
+        <translation type="unfinished">
+割り当て済みのIPアドレスが無効になりました。サーバーを再起動する必要があるかもしれません。</translation>
     </message>
     <message>
         <source>%1 is starting...</source>
@@ -530,6 +540,16 @@ Valid names:
  %1</source>
         <translation>クライアント:
  %1</translation>
+    </message>
+    <message>
+        <source>
+Interface is not active. Unable to start server.</source>
+        <translation type="unfinished">
+インターフェースがアクティブではありません。サーバーを起動できません。</translation>
+    </message>
+    <message>
+        <source>Suggested IP: </source>
+        <translation type="unfinished">推奨IPアドレス: </translation>
     </message>
     <message>
         <source>The Core executable could not be successfully started, although it does exist. Please check if you have sufficient permissions to run this program.</source>
@@ -1231,6 +1251,10 @@ Enabling this setting will disable the server config GUI.</source>
     <message>
         <source>Language</source>
         <translation>言語</translation>
+    </message>
+    <message>
+        <source>Any</source>
+        <translation type="unfinished">どれでも</translation>
     </message>
     <message>
         <source>TLS Certificate Regenerated</source>

translations/deskflow_ru.ts
@@ -374,8 +374,12 @@ Do you want to connect to the server?
         <translation>Не получаеться найти Ip адресc. Проверте подключение к сети.</translation>
     </message>
     <message>
-        <source>Suggested IP: %1</source>
-        <translation>Ваш (рекомендованый) IP адресс: %1</translation>
+        <source>Using IP: </source>
+        <translation type="unfinished">Использование IP-адреса: </translation>
+    </message>
+    <message>
+        <source>Selected as the interface in settings.</source>
+        <translation type="unfinished">Выбран в качестве интерфейса в настройках.</translation>
     </message>
     <message>
         <source>&lt;p&gt;If connecting via the hostname fails, try %1&lt;/p&gt;</source>
@@ -388,6 +392,12 @@ Do you want to connect to the server?
     <message>
         <source>one of the following IPs:&lt;br/&gt;%1</source>
         <translation>один из следующих IP-адресов:&lt;br/&gt;%1</translation>
+    </message>
+    <message>
+        <source>
+A bound IP is now invalid, you may need to restart the server.</source>
+        <translation type="unfinished">
+Привязанный IP-адрес теперь недействителен, возможно, потребуется перезапустить сервер.</translation>
     </message>
     <message>
         <source>%1 is starting...</source>
@@ -534,6 +544,16 @@ Valid names:
  %1</source>
         <translation>Клиент:
  %1</translation>
+    </message>
+    <message>
+        <source>
+Interface is not active. Unable to start server.</source>
+        <translation type="unfinished">
+Интерфейс неактивен. Невозможно запустить сервер.</translation>
+    </message>
+    <message>
+        <source>Suggested IP: </source>
+        <translation type="unfinished">Ваш (рекомендованый) IP адресс: </translation>
     </message>
     <message>
         <source>The Core executable could not be successfully started, although it does exist. Please check if you have sufficient permissions to run this program.</source>
@@ -1239,6 +1259,10 @@ Enabling this setting will disable the server config GUI.</source>
     <message>
         <source>Enable wl-clipboard support</source>
         <translation>Врубить поддержку буфера обмена</translation>
+    </message>
+    <message>
+        <source>Any</source>
+        <translation type="unfinished">Любой</translation>
     </message>
     <message>
         <source>TLS Certificate Regenerated</source>

translations/deskflow_zh_CN.ts
@@ -372,8 +372,12 @@ Do you want to connect to the server?
         <translation>无法检测到 IP 地址。请检查您的网络连接是否正常。</translation>
     </message>
     <message>
-        <source>Suggested IP: %1</source>
-        <translation>建议 IP：%1</translation>
+        <source>Using IP: </source>
+        <translation type="unfinished">使用IP地址： </translation>
+    </message>
+    <message>
+        <source>Selected as the interface in settings.</source>
+        <translation type="unfinished">在设置中选择为接口。</translation>
     </message>
     <message>
         <source>&lt;p&gt;If connecting via the hostname fails, try %1&lt;/p&gt;</source>
@@ -386,6 +390,12 @@ Do you want to connect to the server?
     <message>
         <source>one of the following IPs:&lt;br/&gt;%1</source>
         <translation>以下 IP 地址之一：&lt;br/&gt;%1</translation>
+    </message>
+    <message>
+        <source>
+A bound IP is now invalid, you may need to restart the server.</source>
+        <translation type="unfinished">
+绑定的IP地址现在无效，您可能需要重启服务器。</translation>
     </message>
     <message>
         <source>%1 is starting...</source>
@@ -530,6 +540,16 @@ Valid names:
  %1</source>
         <translation>客户端：
  %1</translation>
+    </message>
+    <message>
+        <source>
+Interface is not active. Unable to start server.</source>
+        <translation type="unfinished">
+接口未激活。无法启动服务器。</translation>
+    </message>
+    <message>
+        <source>Suggested IP: </source>
+        <translation type="unfinished">建议 IP： </translation>
     </message>
     <message>
         <source>The Core executable could not be successfully started, although it does exist. Please check if you have sufficient permissions to run this program.</source>
@@ -1231,6 +1251,10 @@ Enabling this setting will disable the server config GUI.</source>
     <message>
         <source>Language</source>
         <translation>语言</translation>
+    </message>
+    <message>
+        <source>Any</source>
+        <translation type="unfinished">任何</translation>
     </message>
     <message>
         <source>TLS Certificate Regenerated</source>

lockekk/deskflow-hid

translations/deskflow_es.ts
@@ -420,8 +420,16 @@ Do you want to connect to the server?
         <translation type="unfinished">No se puede detectar una dirección IP. Compruebe que su conexión de red esté activa.</translation>
     </message>
     <message>
-        <source>Suggested IP: %1</source>
-        <translation type="unfinished">IP sugerida: %1</translation>
+        <source>Using IP: </source>
+        <translation type="unfinished">Usando IP: </translation>
+    </message>
+    <message>
+        <source>Selected as the interface in settings.</source>
+        <translation type="unfinished">Seleccionado como la interfaz en la configuración.</translation>
+    </message>
+    <message>
+        <source>Suggested IP: </source>
+        <translation type="unfinished">IP sugerida: </translation>
     </message>
     <message>
         <source>&lt;p&gt;If connecting via the hostname fails, try %1&lt;/p&gt;</source>
@@ -434,6 +442,12 @@ Do you want to connect to the server?
     <message>
         <source>one of the following IPs:&lt;br/&gt;%1</source>
         <translation type="unfinished">una de las siguientes IP:&lt;br/&gt;%1</translation>
+    </message>
+    <message>
+        <source>
+A bound IP is now invalid, you may need to restart the server.</source>
+        <translation type="unfinished">
+La dirección IP asignada ahora no es válida; es posible que deba reiniciar el servidor.</translation>
     </message>
     <message>
         <source>&amp;File</source>
@@ -458,6 +472,12 @@ Do you want to connect to the server?
     <message>
         <source>invalid certificate, generating a new one</source>
         <translation type="unfinished">certificado no válido, generando uno nuevo</translation>
+    </message>
+    <message>
+        <source>
+Interface is not active. Unable to start server.</source>
+        <translation type="unfinished">
+La interfaz no está activa. No se puede iniciar el servidor.</translation>
     </message>
     <message>
         <source>%1 will retry in a moment...</source>
@@ -1286,6 +1306,10 @@ Al habilitar esta opción, se deshabilitará la interfaz gráfica de usuario (GU
     <message>
         <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Requires the wl-clipboard package&lt;/p&gt;&lt;p&gt;When using wl-clipboard v2.2.1, there is a focus stealing bug that may make Deskflow harder to use. This has been fixed when using the wl-clipboard master branch, unless your Compositor lacks wlroots-data-control protocol support.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
         <translation type="unfinished">&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Requiere el paquete wl-clipboard&lt;/p&gt;&lt;p&gt;Al usar wl-clipboard v2.2.1, existe un error que provoca la pérdida del foco y que puede dificultar el uso de Deskflow. Este error se ha corregido al usar la rama principal de wl-clipboard, a menos que su Compositor no sea compatible con el protocolo wlroots-data-control.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+    </message>
+    <message>
+        <source>Automatic</source>
+        <translation type="unfinished">Automática</translation>
     </message>
 </context>
 <context>

translations/deskflow_it.ts
@@ -408,8 +408,16 @@ Vuoi connetterti al server?
         <translation>Impossibile rilevare un indirizzo IP. Controlla che la tua connessione di rete sia attiva.</translation>
     </message>
     <message>
-        <source>Suggested IP: %1</source>
-        <translation>IP suggerito: %1</translation>
+        <source>Using IP: </source>
+        <translation type="unfinished">Utilizzo dell&apos;indirizzo IP: </translation>
+    </message>
+    <message>
+        <source>Selected as the interface in settings.</source>
+        <translation type="unfinished">Selezionata come interfaccia nelle impostazioni.</translation>
+    </message>
+    <message>
+        <source>Suggested IP: </source>
+        <translation type="unfinished">IP suggerito: </translation>
     </message>
     <message>
         <source>&lt;p&gt;If connecting via the hostname fails, try %1&lt;/p&gt;</source>
@@ -422,6 +430,12 @@ Vuoi connetterti al server?
     <message>
         <source>one of the following IPs:&lt;br/&gt;%1</source>
         <translation>uno dei seguenti IP:&lt;br/&gt;%1</translation>
+    </message>
+    <message>
+        <source>
+A bound IP is now invalid, you may need to restart the server.</source>
+        <translation type="unfinished">
+L&apos;indirizzo IP associato non è più valido, potrebbe essere necessario riavviare il server.</translation>
     </message>
     <message>
         <source>&amp;File</source>
@@ -466,6 +480,12 @@ Nomi validi:
     <message>
         <source>invalid certificate, generating a new one</source>
         <translation type="unfinished">certificato non valido, ne viene generato uno nuovo</translation>
+    </message>
+    <message>
+        <source>
+Interface is not active. Unable to start server.</source>
+        <translation type="unfinished">
+L&apos;interfaccia non è attiva. Impossibile avviare il server.</translation>
     </message>
     <message>
         <source>%1 will retry in a moment...</source>
@@ -1286,6 +1306,10 @@ L&apos;abilitazione di questa impostazione disabiliterà l&apos;interfaccia graf
     <message>
         <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Requires the wl-clipboard package&lt;/p&gt;&lt;p&gt;When using wl-clipboard v2.2.1, there is a focus stealing bug that may make Deskflow harder to use. This has been fixed when using the wl-clipboard master branch, unless your Compositor lacks wlroots-data-control protocol support.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
         <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Richiede il pacchetto wl-clipboard&lt;/p&gt;&lt;p&gt;Quando si utilizza wl-clipboard v2.2.1, si verifica un bug di furto del focus che potrebbe rendere Deskflow più difficile da usare. Questo problema è stato risolto quando si utilizza il ramo master di wl-clipboard, a meno che il proprio compositore non supporti il ​​protocollo wlroots-data-control.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+    </message>
+    <message>
+        <source>Automatic</source>
+        <translation type="unfinished">Automatica</translation>
     </message>
 </context>
 <context>

translations/deskflow_ja.ts
@@ -372,8 +372,12 @@ Do you want to connect to the server?
         <translation>IPアドレスが見つかりません。ネットワーク接続を確認してください。</translation>
     </message>
     <message>
-        <source>Suggested IP: %1</source>
-        <translation>推奨IPアドレス: %1</translation>
+        <source>Using IP: </source>
+        <translation type="unfinished">IPアドレスを使用する： </translation>
+    </message>
+    <message>
+        <source>Selected as the interface in settings.</source>
+        <translation type="unfinished">設定でインターフェースとして選択されています。</translation>
     </message>
     <message>
         <source>&lt;p&gt;If connecting via the hostname fails, try %1&lt;/p&gt;</source>
@@ -386,6 +390,12 @@ Do you want to connect to the server?
     <message>
         <source>one of the following IPs:&lt;br/&gt;%1</source>
         <translation>以下のIPアドレスのいずれか:&lt;br/&gt;%1</translation>
+    </message>
+    <message>
+        <source>
+A bound IP is now invalid, you may need to restart the server.</source>
+        <translation type="unfinished">
+割り当て済みのIPアドレスが無効になりました。サーバーを再起動する必要があるかもしれません。</translation>
     </message>
     <message>
         <source>%1 is starting...</source>
@@ -530,6 +540,16 @@ Valid names:
  %1</source>
         <translation>クライアント:
  %1</translation>
+    </message>
+    <message>
+        <source>
+Interface is not active. Unable to start server.</source>
+        <translation type="unfinished">
+インターフェースがアクティブではありません。サーバーを起動できません。</translation>
+    </message>
+    <message>
+        <source>Suggested IP: </source>
+        <translation type="unfinished">推奨IPアドレス: </translation>
     </message>
     <message>
         <source>The Core executable could not be successfully started, although it does exist. Please check if you have sufficient permissions to run this program.</source>
@@ -1287,6 +1307,10 @@ Enabling this setting will disable the server config GUI.</source>
     <message>
         <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Requires the wl-clipboard package&lt;/p&gt;&lt;p&gt;When using wl-clipboard v2.2.1, there is a focus stealing bug that may make Deskflow harder to use. This has been fixed when using the wl-clipboard master branch, unless your Compositor lacks wlroots-data-control protocol support.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
         <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;wl-clipboard パッケージが必要です。&lt;/p&gt;&lt;p&gt;wl-clipboard v2.2.1 を使用すると、フォーカス盗用のバグにより Deskflow の使い勝手が悪くなる可能性があります。この問題は wl-clipboard のマスターブランチで修正されていますが、使用しているコンポジターが wlroots-data-control プロトコルに対応している必要があります。&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+    </message>
+    <message>
+        <source>Automatic</source>
+        <translation type="unfinished">自動</translation>
     </message>
 </context>
 <context>

translations/deskflow_ru.ts
@@ -374,8 +374,12 @@ Do you want to connect to the server?
         <translation>Не получаеться найти Ip адресc. Проверте подключение к сети.</translation>
     </message>
     <message>
-        <source>Suggested IP: %1</source>
-        <translation>Ваш (рекомендованый) IP адресс: %1</translation>
+        <source>Using IP: </source>
+        <translation type="unfinished">Использование IP-адреса: </translation>
+    </message>
+    <message>
+        <source>Selected as the interface in settings.</source>
+        <translation type="unfinished">Выбран в качестве интерфейса в настройках.</translation>
     </message>
     <message>
         <source>&lt;p&gt;If connecting via the hostname fails, try %1&lt;/p&gt;</source>
@@ -388,6 +392,12 @@ Do you want to connect to the server?
     <message>
         <source>one of the following IPs:&lt;br/&gt;%1</source>
         <translation>один из следующих IP-адресов:&lt;br/&gt;%1</translation>
+    </message>
+    <message>
+        <source>
+A bound IP is now invalid, you may need to restart the server.</source>
+        <translation type="unfinished">
+Привязанный IP-адрес теперь недействителен, возможно, потребуется перезапустить сервер.</translation>
     </message>
     <message>
         <source>%1 is starting...</source>
@@ -534,6 +544,16 @@ Valid names:
  %1</source>
         <translation>Клиент:
  %1</translation>
+    </message>
+    <message>
+        <source>
+Interface is not active. Unable to start server.</source>
+        <translation type="unfinished">
+Интерфейс неактивен. Невозможно запустить сервер.</translation>
+    </message>
+    <message>
+        <source>Suggested IP: </source>
+        <translation type="unfinished">Ваш (рекомендованый) IP адресс: </translation>
     </message>
     <message>
         <source>The Core executable could not be successfully started, although it does exist. Please check if you have sufficient permissions to run this program.</source>
@@ -1291,6 +1311,10 @@ Enabling this setting will disable the server config GUI.</source>
     <message>
         <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Requires the wl-clipboard package&lt;/p&gt;&lt;p&gt;When using wl-clipboard v2.2.1, there is a focus stealing bug that may make Deskflow harder to use. This has been fixed when using the wl-clipboard master branch, unless your Compositor lacks wlroots-data-control protocol support.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
         <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Для этого необходим пакет wl-clipboard.&lt;/p&gt;&lt;p&gt;Когда ты используешь wl-clipboard v2.2.1. Возникает ошибка перехвата фокуса, которая мешает использовать deskflowю. Это фиксется если использовать wl-clipboard из ветки master, если только ваш Compositor не поддерживает протокол wlroots-data-control.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+    </message>
+    <message>
+        <source>Automatic</source>
+        <translation type="unfinished">Автоматический</translation>
     </message>
 </context>
 <context>

translations/deskflow_zh_CN.ts
@@ -372,8 +372,12 @@ Do you want to connect to the server?
         <translation>无法检测到 IP 地址。请检查您的网络连接是否正常。</translation>
     </message>
     <message>
-        <source>Suggested IP: %1</source>
-        <translation>建议 IP：%1</translation>
+        <source>Using IP: </source>
+        <translation type="unfinished">使用IP地址： </translation>
+    </message>
+    <message>
+        <source>Selected as the interface in settings.</source>
+        <translation type="unfinished">在设置中选择为接口。</translation>
     </message>
     <message>
         <source>&lt;p&gt;If connecting via the hostname fails, try %1&lt;/p&gt;</source>
@@ -386,6 +390,12 @@ Do you want to connect to the server?
     <message>
         <source>one of the following IPs:&lt;br/&gt;%1</source>
         <translation>以下 IP 地址之一：&lt;br/&gt;%1</translation>
+    </message>
+    <message>
+        <source>
+A bound IP is now invalid, you may need to restart the server.</source>
+        <translation type="unfinished">
+绑定的IP地址现在无效，您可能需要重启服务器。</translation>
     </message>
     <message>
         <source>%1 is starting...</source>
@@ -530,6 +540,16 @@ Valid names:
  %1</source>
         <translation>客户端：
  %1</translation>
+    </message>
+    <message>
+        <source>
+Interface is not active. Unable to start server.</source>
+        <translation type="unfinished">
+接口未激活。无法启动服务器。</translation>
+    </message>
+    <message>
+        <source>Suggested IP: </source>
+        <translation type="unfinished">建议 IP： </translation>
     </message>
     <message>
         <source>The Core executable could not be successfully started, although it does exist. Please check if you have sufficient permissions to run this program.</source>
@@ -1287,6 +1307,10 @@ Enabling this setting will disable the server config GUI.</source>
     <message>
         <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Requires the wl-clipboard package&lt;/p&gt;&lt;p&gt;When using wl-clipboard v2.2.1, there is a focus stealing bug that may make Deskflow harder to use. This has been fixed when using the wl-clipboard master branch, unless your Compositor lacks wlroots-data-control protocol support.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
         <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;需要 wl-clipboard 包&lt;/p&gt;&lt;p&gt;使用 wl-clipboard v2.2.1 时存在一个焦点抢夺 Bug，可能导致 Deskflow 使用不便。该问题已在 wl-clipboard 的 master 分支中修复，除非您的合成器不支持 wlroots-data-control 协议。&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+    </message>
+    <message>
+        <source>Automatic</source>
+        <translation type="unfinished">自动的</translation>
     </message>
 </context>
 <context>